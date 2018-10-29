7899219d97f2e899dec617dfa1896b0a2d291d18
danielkaczmarczyk <danielkaczmarczyk2@gmail.com>
2018-10-29 17:34:04 +0000

    edits

---
+++ 10-mvc-sinatra/restaurant/app/controllers/dishes_controller.rb
@@ -0,0 +1,13 @@
+class DishesController < ApplicationController
+
+  get "/dishes" do
+    @dishes = Dish.all.sort_by {|dish| dish.price}
+    erb :"dishes/index"
+  end
+
+  get "/dishes/:id" do
+    @dish = Dish.find(params[:id])
+    erb :"dishes/show"
+  end
+
+end

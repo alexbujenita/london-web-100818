08af69332372a5a506a18c6f906259b0ed08b3d2
danielkaczmarczyk <danielkaczmarczyk2@gmail.com>
2018-10-31 15:14:21 +0000

    edits

---
+++ 12-sinatra-relationships/catapp/app/controllers/humans_controller.rb
@@ -0,0 +1,24 @@
+class HumansController < ApplicationController
+
+  get "/humans" do
+    @humans = Human.all
+    erb :"humans/index"
+  end
+
+  get "/humans/new" do
+    @cats = Cat.all
+    erb :"humans/new"
+  end
+
+  post "/humans" do
+    binding.pry
+    human = Human.create(params[:human])
+    redirect "/humans/#{human.id}"
+  end
+
+  get "/humans/:id" do
+    @human = Human.find(params[:id])
+    erb :"humans/show"
+  end
+
+end

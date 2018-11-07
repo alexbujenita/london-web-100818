af02cccab30a3f41eaf89c53ee381de2c13de061
danielkaczmarczyk <danielkaczmarczyk2@gmail.com>
2018-11-07 14:44:02 +0000

    edits

--- 17-rails-forms-validations/docoffice/app/controllers/doctors_controller.rb
+++ 17-rails-forms-validations/docoffice/app/controllers/doctors_controller.rb
@@ -14,23 +14,33 @@ class DoctorsController < ApplicationController
   end
 
   def create
-    byebug
     @doctor = Doctor.create(doctor_params)
-    byebug
-    redirect_to doctor_path(@doctor)
+    if @doctor.valid?
+      redirect_to doctor_path(@doctor)
+    else
+      flash[:errors] = @doctor.errors.full_messages
+      redirect_to new_doctor_path
+    end
   end
 
   def edit
+    @offices = Office.all
+    @patients = Patient.all
   end
 
   def update
     @doctor.update(doctor_params)
-    redirect doctor
+    if @doctor.valid?
+      redirect_to @doctor
+    else
+      flash[:errors] = @doctor.errors.full_messages
+      redirect_to edit_doctor_path(@doctor)
+    end
   end
 
   def destroy
     @doctor.destroy
-    redirect doctors_path
+    redirect_to doctors_path
   end
 
   private
--- 17-rails-forms-validations/docoffice/app/models/doctor.rb
+++ 17-rails-forms-validations/docoffice/app/models/doctor.rb
@@ -2,4 +2,7 @@ class Doctor < ApplicationRecord
   belongs_to :office
   has_many :visits
   has_many :patients, through: :visits
+
+  validates :name, uniqueness: true, presence: true, length: { minimum: 2 }
+  validates :specialty, presence: true, length: { minimum: 2 }
 end

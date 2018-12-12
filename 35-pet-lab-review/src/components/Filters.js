4bb6f909e40b51c3f6a9fbbd22f47e4d003c4c2f
MinimumViablePerson <nicolas.marcora@yahoo.co.uk>
2018-12-12 16:33:39 +0000

    pet lab review

---
+++ 35-pet-lab-review/src/components/Filters.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+class Filters extends React.Component {
+  render () {
+    const { handleChange, handleClick } = this.props
+    // const handleChange = this.props.handleChange
+    return (
+      <div className='ui form'>
+        <h3>Animal type</h3>
+        <div className='field'>
+          <select onChange={handleChange} name='type' id='type'>
+            <option value='all'>All</option>
+            <option value='cat'>Cats</option>
+            <option value='dog'>Dogs</option>
+            <option value='micropig'>Micropigs</option>
+          </select>
+        </div>
+
+        <div className='field'>
+          <button onClick={handleClick} className='ui secondary button'>Find pets</button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Filters
---
+++ 35-pet-lab-review/src/components/Pet.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Pet = ({ pet, handleClick }) =>
+  <div className='card'>
+    <div className='content'>
+      <a className='header'>
+        {
+          pet.gender === 'female'
+            ? '♀'
+            : '♂'
+        }
+        { pet.name }
+      </a>
+      <div className='meta'>
+        <span className='date'>{ pet.type }</span>
+      </div>
+      <div className='description'>
+        <p>Age: { pet.age }</p>
+        <p>Weight: { pet.weight }</p>
+      </div>
+    </div>
+    <div className='extra content'>
+      {
+        pet.isAdopted
+          ? <button className='ui disabled button'>Already adopted</button>
+          : <button onClick={handleClick} className='ui primary button'>Adopt pet</button>
+      }
+    </div>
+  </div>
+
+export default Pet
